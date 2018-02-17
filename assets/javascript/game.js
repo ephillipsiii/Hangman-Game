399d84d5517f51955027a218445ac82d6373d783
Lappy Mc Lap Face <ephillipsiii>
2018-02-16 20:35:26 -0700

    html and beginning js logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+var wordPool = ['Capra Demon', 'Nito', 'Gwyn, Lord of Cinder', 'Ornstein and Smough', 'Seath the Scaleless', 'Yhorm the Giant', 'Aldrich, Devourer of Gods' ];
+    
+var rand = wordPool[Math.floor(Math.random() * wordPool.length)];
+    document.write(rand);
+    
+    // choose a word from wordPool - DONE!
+
+    //choose word from random ON KEY PRESS, 
+    //have random word appear as blanks.
+
+    // log chosen incorrect letters in document/html
+    //log chosen correct letters IN PLACE of blanks 
+
+    //log choices left
+    // log wins/losses
+
+
+
+
+
+
+
+
+// var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// for alphabet 
