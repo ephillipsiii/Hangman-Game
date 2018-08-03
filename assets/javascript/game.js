a21cc788718c85d48fdbbf7685c6032cc86ee26b
Edford Phillips <ed.d.phillips.iii@gmail.com>
2018-08-03 14:26:56 -0600

    updated game to be beer related

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 // var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
     //pool of bosses to be chosen at random 
-var wordPool = ['Capra Demon', 'Nito', 'Gwyn Lord of Cinder', 'Ornstein and Smough', 'Seath the Scaleless', 'Yhorm the Giant', 'Aldrich Devourer of Gods' ];
+var wordPool = ['IPA', 'Brown', 'Stout', 'FlandersRed', 'RedAle', 'BlackIPA', 'Pilsener' ];
 
 
 document.onkeyup = function(event){
